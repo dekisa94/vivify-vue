bdcf2edb423b89b5b73de33f5304a4b167851a49
dekisa94 <dsimic94@gmail.com>
2018-04-23 21:00:09 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ContactsList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContactsList from './components/ContactsList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ContactsList
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/ContactsList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <table>
+      <thead>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Email</th>
+      </thead>
+      <tbody>
+        <tr v-for= "(user,key) in users" :key="key">
+        <td> {{user.firstName}} </td>
+        <td> {{user.lastName}} </td>
+        <td> {{user.email}} </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactsList',
+  data() {
+   return{
+     users: [
+       {firstName: 'John', lastName: 'Doe', email: 'john@mail.com'},
+       {firstName: 'Joan', lastName: 'Conol', email: 'joan@mail.com'},
+       {firstName: 'Mike', lastName: 'Daglas', email: 'mike@mail.com'}
+     ]
+   }
+  }
+}
+</script>

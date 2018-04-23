f106de40da9ebd39643e163af36689d2a78f7060
dekisa94 <dsimic94@gmail.com>
2018-04-23 21:12:27 +0200

    add_v_if

--- src/components/ContactsList.vue
+++ src/components/ContactsList.vue
@@ -7,10 +7,10 @@
       <th>Email</th>
       </thead>
       <tbody>
-        <tr v-for= "(user,key) in users" :key="key">
-        <td> {{user.firstName}} </td>
-        <td> {{user.lastName}} </td>
-        <td> {{user.email}} </td>
+        <tr v-for= "(contact,key) in contacts" :key="key" v-if="contact.firstName==='John'">
+        <td> {{contact.firstName}} </td>
+        <td> {{contact.lastName}} </td>
+        <td> {{contact.email}} </td>
         </tr>
       </tbody>
     </table>
@@ -22,7 +22,7 @@ export default {
   name: 'ContactsList',
   data() {
    return{
-     users: [
+     contacts: [
        {firstName: 'John', lastName: 'Doe', email: 'john@mail.com'},
        {firstName: 'Joan', lastName: 'Conol', email: 'joan@mail.com'},
        {firstName: 'Mike', lastName: 'Daglas', email: 'mike@mail.com'}

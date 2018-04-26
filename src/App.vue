c74b91bba2aada08f1580a072855ec8dd5a78ee0
dekisa94 <dsimic94@gmail.com>
2018-04-26 19:18:23 +0200

    computed, watch..

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <div id="app">
     <ContactsList />
+    <PropsExample
+    :message="message"
+    @notify-parent="handleNotifyParent"
+    />
   </div>
 </template>
 
 <script>
 import ContactsList from './components/ContactsList.vue'
+import PropsExample from './components/PropsExample.vue'
 
 export default {
   name: 'app',
   components: {
-    ContactsList
+    ContactsList,
+    PropsExample
+  },
+  data(){
+    return{
+      message: "Hello"
+    }
+  },
+  created(){
+    setTimeout(() => {
+      this.message="new Message"
+    }, 3000)
+  },
+  methods:{
+    handleNotifyParent(message){
+      this.message=message
+    }
   }
 }
 </script>
---
+++ src/components/PropsExample.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div>
+            {{message}}
+        </div>
+        <div>
+            {{reversedMessage}}
+        </div>
+        <input type="text" v-model="searchTerm" />
+        <button @click="notifyParent">Notify parent</button>
+    </div>
+</template>
+
+<script>
+export default {
+  props:['message'],
+  data(){
+      return{
+          searchTerm: ''
+      }
+  },
+  methods:{
+      notifyParent(){
+          this.$emit('notify-parent', 'Hello from child!')
+      }
+  },
+  computed:{
+    reversedMessage(){
+      return this.message.split('').reverse().join('')
+    }
+  },
+  watch:{
+      searchTerm(newValue, oldValue){
+          console.log('New value:', newValue)
+          console.log('Old Value:', oldValue)
+      }
+  }
+}
+</script>
+
